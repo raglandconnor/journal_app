0768c8a6cbcaa325b54484ca58b4b70a0fa023df
raglandconnor <raglandconnor@ufl.edu>
2024-06-20 08:40:22 -0400

    Connect to mongoDB, add .env and envalid

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,15 @@
-import express from "express";
-const app = express();
-const port = 3500;
+import app from "./app";
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-});
+const port = env.PORT;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+mongoose
+    .connect(env.MONGO_CONNECTION_STRING)
+    .then(() => {
+        console.log("Mongoose connected");
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch(console.error);
