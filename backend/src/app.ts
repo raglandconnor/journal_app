e6c431236f3d35e62d8ce8289f3037c8a5e4940b
raglandconnor <raglandconnor@ufl.edu>
2024-06-22 08:28:32 -0400

    #4: Create schema, create basic middleware in app.ts, and implement the error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,34 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import JournalEntryModel from "./models/journalEntry";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+app.get("/", async (req, res, next) => {
+    // Use try catch to handle errors
+    try {
+        const journalEntries = await JournalEntryModel.find().exec();
+        res.status(200).json(journalEntries);
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.use((req, res, next) => {
+    next(Error("Endpoint not found"));
+});
+
+// Error handler
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.log(error);
+    let errorMsg = "An unknown error occurred";
+    if (error instanceof Error) {
+        errorMsg = error.message;
+    }
+
+    res.status(500).json({
+        error: errorMsg,
+    });
 });
 
 export default app;
---
+++ backend/src/models/journalEntry.ts
@@ -0,0 +1,18 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const journalEntrySchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        text: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+type JournalEntry = InferSchemaType<typeof journalEntrySchema>;
+
+export default model<JournalEntry>("JournalEntry", journalEntrySchema);
