078c9eaf28002a29d3cb301a644ec21cd6475580
raglandconnor <raglandconnor@ufl.edu>
2024-06-27 07:46:05 -0400

    If user refreshes after cookie expires they are brought to the login page

--- frontend/src/pages/JournalsRoot.tsx
+++ frontend/src/pages/JournalsRoot.tsx
@@ -3,17 +3,21 @@ import JournalsPage from "./JournalsPage";
 import Navbar from "./Navbar";
 import { UserModel } from "../models/userModel";
 import * as UserAPI from "../api/userAPI";
+import { useNavigate } from "react-router-dom";
 
 function JournalsRoot() {
     const [user, setUser] = useState<UserModel | null>(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function fetchUser() {
             try {
                 const fetchedUser = await UserAPI.getLoggedInUser();
+
                 setUser(fetchedUser);
             } catch (error) {
                 console.error(error);
+                navigate("/login");
             }
         }
 
